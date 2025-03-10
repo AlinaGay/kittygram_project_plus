4a806144c319d926e0b211f89b6831f936dc891b
AlinaGay <gayfullina.a@gmail.com>
2025-03-10 12:36:58 +0100

    Add model and serializer for achievement

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Achievement, Cat, Owner
 
 
 class CatSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField(read_only=True)
+    achievements = serializers.StringRelatedField(read_only=True, many=True)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fileds = ('id', 'name')
